b1b798a2f78da29612fb8c8d7d89017ce699398d
Emmanuel <manumtz20@gmail.com>
2024-05-29 19:06:25 -0600

    responsive home y navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,9 @@ import styles from './NavBar.module.scss';
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
-      <ul className={styles.list}>
-        <li>Mas Proyectos</li>
-        <li>Mas Sobre Mi</li>
-      </ul>
-      <button className={styles.button}>Descargar CV</button>
+      <span>Mas proyectos</span>
+      <span>Mas sobre mi</span>
+      <button>Descargar CV</button>
     </nav>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,15 +6,17 @@ const HomePage = () => {
     <div className={styles.aboutWrapper}>
       <section className={styles.about}>
         <img className={styles.foto} src={foto} alt="foto de perfil" />
-        <h1>Emmanuel Martinez Zamudio</h1>
-        <h2>Full Stack Developer | Front-end & Back-end</h2>
-        <p>Actualmente llevo 2 años en el desarrollo IT</p>
-        <p>
-          Desde chico me a gustado pasar mi tiempo libre navegando por la web, chateando o escuchando
-          música, y en todo ese tiempo me eh dado cuenta de la evolución que han tenido los programas,
-          aplicación y paginas web, es por eso que me gustaría ser yo ahora quien brinde esas
-          experiencias y herramientas
-        </p>
+        <div>
+          <h1>Emmanuel Martinez Zamudio</h1>
+          <h2>Full Stack Developer | Front-end & Back-end</h2>
+          <p>Actualmente llevo 2 años en el desarrollo IT</p>
+          <p>
+            Desde chico me a gustado pasar mi tiempo libre navegando por la web, chateando o escuchando
+            música, y en todo ese tiempo me eh dado cuenta de la evolución que han tenido los programas,
+            aplicación y paginas web, es por eso que me gustaría ser yo ahora quien brinde esas
+            experiencias y herramientas
+          </p>
+        </div>
       </section>
     </div>
   );
