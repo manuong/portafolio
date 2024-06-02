2f5f2829ffab415c4eb82e78714fb5531bc4289d
Emmanuel <manumtz20@gmail.com>
2024-06-01 18:01:39 -0600

    estilos en tarjeta de projectos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,13 +4,15 @@ const ProjectCard = ({ foto, name, description, additional, technologies, demo, 
   return (
     <article className={styles.wrapperCard}>
       <img src={foto} alt="imagen del proyecto" />
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>{additional}</p>
-      <p>{technologies}</p>
-      <div>
-        <a href={demo}>Demo</a>
-        <a href={github}>Repositorio</a>
+      <div className={styles.content}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        {additional && <p>{additional}</p>}
+        <p>{technologies}</p>
+        <div>
+          <a href={demo}>Demo</a>
+          <a href={github}>Repositorio</a>
+        </div>
       </div>
     </article>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,30 @@
 import styles from './Projects.module.scss';
+import data from '../data/projects.json';
 import ProjectCard from './ProjectCard';
-import foto1 from '../assets/projectos/care-with-love.jpeg';
-import foto2 from '../assets/projectos/countries.jpeg';
-import foto3 from '../assets/projectos/notes-api.jpeg';
 
 const Projects = () => {
   return (
     <section className={styles.wrapperSection}>
       <h2>Projectos</h2>
       <div className={styles.wrapperCards}>
-        <ProjectCard
-          foto={foto1}
-          name={'Care With Love'}
-          description={
-            'Página web desarrollada en equipo donde su principal objetivo es conectar clientes con proveedores para el cuidado de un adulto mayor; incluye: método de pago, autenticación con terceros, dashboard, chat en vivo, y más funcionalidades'
-          }
-          additional={
-            'Trabajé en el back end diseñando rutas que incluía filtros y ordenamiento, manejo de errores y token de seguridad'
-          }
-          technologies={'React, Redux, SASS, Express, Sequelize, PostgreSQL'}
-          demo={'https://proyecto-final-front-ashy.vercel.app'}
-          github={'https://github.com/pablo0261/proyecto-final-front'}
-        />
-        <ProjectCard foto={foto2} />
-        <ProjectCard foto={foto3} />
+        <div>
+          {data.map((project, index) => {
+            const { foto, name, description, additional, technologies, demo, github } = project;
+
+            return (
+              <ProjectCard
+                key={index}
+                foto={foto}
+                name={name}
+                description={description}
+                additional={additional}
+                technologies={technologies}
+                demo={demo}
+                github={github}
+              />
+            );
+          })}
+        </div>
       </div>
     </section>
   );
