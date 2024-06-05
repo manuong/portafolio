f8b16409ba5282c9cc98e15854feeeb2d0c61002
Emmanuel <manumtz20@gmail.com>
2024-06-05 15:16:30 -0600

    actualizacion de estilos en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
+import ButtonCV from './ButtonCV';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
-    <nav className={styles.navbar}>
-      <span>Mas proyectos</span>
-      <span>Mas sobre mi</span>
-      <button>Descargar CV</button>
-    </nav>
+    <header className={styles.wrapperHeader}>
+      <nav className={styles.navbar}>
+        <div className={styles.name}>
+          manuong<span>.dev</span>
+        </div>
+        <div className={styles.contacts}>
+          <span>manumtz.job@gmail.com</span>
+          <span>+52 56 1282 8043</span>
+          <span>in/manuong</span>
+          <ButtonCV name={'Descargar CV'} />
+        </div>
+      </nav>
+    </header>
   );
 };
 
