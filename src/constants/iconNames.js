28ecb5eb18a1161f4240a689d77ea919d858d19a
Emmanuel <manumtz20@gmail.com>
2024-06-06 18:50:04 -0600

    declaracion de constantes

---
+++ src/constants/iconNames.js
@@ -0,0 +1,11 @@
+const ICON_NAMES = {
+  INSTAGRAM: 'logo-instagram',
+  FACEBOOK: 'logo-facebook',
+  GMAIL: 'logo-google',
+  WHATSAPP: 'logo-whatsapp',
+  LINKEDIN: 'logo-linkedin',
+  DISCORD: 'logo-discord',
+  GITHUB: 'logo-github',
+};
+
+export default ICON_NAMES;
---
+++ src/constants/urlContacts.js
@@ -0,0 +1,12 @@
+const CONTACTS = {
+  INSTAGRAM: 'https://www.instagram.com/_manuong/',
+  FACEBOOK: 'https://www.facebook.com/profile.php?id=61557007523987',
+  SPOTIFY: 'https://open.spotify.com/playlist/1TO5Q0dWZSm4sHJ4LDUfqe?si=d591ee5dbb404016',
+  GMAIL: 'mailto:manumtz.job@gmail.com',
+  WHATSAPP: 'https://wa.me/5215612828043',
+  LINKEDIN: 'https://www.linkedin.com/in/manuong',
+  DISCORD: 'https://discord.gg/vbWjxCkkxM',
+  GITHUB: 'https://github.com/manuong',
+};
+
+export default CONTACTS;
